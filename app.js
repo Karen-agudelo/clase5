010f9bcbb89ec5793d887cc3919419e045b0323c
Karen-agudelo <karenagudelo40@gmail.com>
2021-12-22 00:44:22 +0100

    primera prueba

---
+++ app.js
@@ -0,0 +1,30 @@
+ let fs = require('fs')
+let tareas = fs.readFileSync(__dirname + '/tareas.json', 'utf-8');
+let datosConvertidos = JSON.parse(tareas);
+
+let prueba = process.argv[2];
+
+switch(prueba){
+    case "listar":
+        console.log(datosConvertidos);
+        break;
+    case undefined:
+        console.log("Atención - Tienes que pasar una acción.");
+        break;
+    default:
+        console.log("No entiendo qué quieres hacer.");0
+        break;
+}
+ 
+/* Repetir como un loro
+Vamos a programar una función que se comporte como un loro. Sí, todo lo que "oye", ¡lo repite!
+
+La función debe llamarse loro y debe recibir como parámetro el texto y repetirlo 5 veces por consola. */
+/* let fechaActual = new Date ();
+let anio = fechaActual.getFullYear();
+let mes = fechaActual.getMonth();
+let dia = fechaActual.getDate();
+
+
+console.log("Día: "+fechaActual.getDate())
+console.log('Hoy es el día '+ dia+' del mes '+ mes+' del año '+anio) */
---
+++ repaso.js
@@ -0,0 +1,4 @@
+/* Ahora te pedimos que declares una función llamada  tripleDeLaSuma() que reciba dos parámetros. 
+Luego tiene que sumar ambos y devolvernos el triple del valor del resultado de sumar ambos parámetros . 
+Para ello ya contás (aunque no la veas declarada) con la función triple, que recibe un parámetro y 
+retorna el valor del mismo multiplicado por tres. */
